e59bb1c6774788f41cd1d48efb0bad85ce0163c4
tadicodes <tadishongedza@gmail.com>
2024-05-14 07:01:15 -0700

    commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,52 +1,44 @@
 #!/usr/bin/python3
-'''Shows possible arrangements of N-queens on a chess board'''
+""" N queens """
 import sys
 
-def is_valid(board, row, col):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
 
-def solve_n_queens(board, col):
-    if col == len(board):
-        for row in range(len(board)):
-            solution = []
-            for col in range(len(board)):
-                if board[row][col] == 1:
-                    solution.append([row, col])
-            print(solution)
-        return True
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    found_solution = False
-    for row in range(len(board)):
-        if is_valid(board, row, col):
-            board[row][col] = 1
-            found_solution = solve_n_queens(board, col + 1) or found_solution
-            board[row][col] = 0
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    return found_solution
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+n = int(sys.argv[1])
 
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
 
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solve_n_queens(board, 0)
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
